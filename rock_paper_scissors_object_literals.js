6803fc14643748eecdaf0e7cb8e12a8094fa1b72
Marc <gopherproxy@gmail.com>
2021-10-27 11:41:32 +0200

    Object literal notation

--- rock_paper_scissors_object_literals.js
+++ rock_paper_scissors_object_literals.js
@@ -2,21 +2,67 @@
 // https://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
 
 var myGame = {
-	// In object literal notation, an object is described as a set of comma-separated name/value pairs enclosed in curly braces.
-	// Names inside the object may be either strings or identifiers that are followed by a colon.
-	 // object literals can contain properties and methods:
-	
-	variableKey : variableValue,
-	
-	// a method 
-	functionkey: function () {
-		'use strict';
+	// object properties
+	computerChoice: '',
+	userChoice: '',
+
+	// object methods
+	computerInput() {
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+				this.computerChoice = 'Rock';
+				break;
+			case 1:
+				this.computerChoice = 'Paper';
+				break;
+			case 2:
+				this.computerChoice = 'Scissors';
+				break;
+		}
+		console.log('Computer: ' + this.computerChoice);
 	},
 
-	///// module end /////
+	compare() {
+		let showResult = document.getElementById('result')
+		if (this.userChoice === this.computerChoice) {
+			showResult.innerHTML = 'Game is a draw!';
+		} else if (this.userChoice === 'Paper' && this.computerChoice === 'Rock') {
+			showResult.innerHTML = 'You win!';
+		} else if (this.userChoice === 'Paper' && this.computerChoice === 'Scissors') {
+			showResult.innerHTML = 'Computer wins!';
+		} else if (this.userChoice === 'Scissors' && this.computerChoice === 'Rock') {
+			showResult.innerHTML = 'Computer wins!';
+		} else if (this.userChoice === 'Scissors' && this.computerChoice === 'Paper') {
+			showResult.innerHTML = 'You win!';
+		} else if (this.userChoice === 'Rock' && this.computerChoice === 'Scissors') {
+			showResult.innerHTML = 'You win!';
+		} else if (this.userChoice === 'Rock' && this.computerChoice === 'Paper') {
+			resultDisplay.innerHTML = 'Computer wins!';
+		}
+	},
+
+	display() {
+		// display player's choice
+		let showPlayerChoice = document.getElementById('player_choice');
+		showPlayerChoice.innerHTML = this.userChoice;
+		// display computer choice
+		let showComputerChoice = document.getElementById('computer_choice');
+		showComputerChoice.innerHTML = this.computerChoice;
+	}
+
+	///// object end /////
 };
 
+var buttons = document.getElementsByClassName('button');
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', play)
+}
 
+// Game Execution
 function play() {
-	myGame.functionkey();
+	myGame.userChoice = this.id;
+	console.log('User: ' + myGame.userChoice);
+	myGame.computerInput();
+	myGame.compare();
+	myGame.display();
 }
