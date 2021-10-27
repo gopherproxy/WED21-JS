7bfc9e12bbc81f0cc7d16f8ec9b81deec09deed0
Marc <gopherproxy@gmail.com>
2021-10-27 13:56:16 +0200

    Module pattern

--- rock_paper_scissors_module.js
+++ rock_paper_scissors_module.js
@@ -1,25 +1,69 @@
 var myGame = (function () {
 
-	// private variables
-	var myVar = "some value or expression";
+	// "private" variables (only visible in module scope)
+	var computerChoice, userChoice, showUserChoice, showComputerChoice, showResult;
+
+	showResult = document.getElementById('result');
+	showUserChoice = document.getElementById('player_choice');
+	showComputerChoice = document.getElementById('computer_choice');
 
 	// functions
-	var myPrivateFunction;
-	
+	var computerInput, compare, display;
 
-	// private functions
-	myPrivateFunction = function() {
-	
+	// private functions  (only visible in module scope)
+	computerInput = function () {
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+				computerChoice = 'Rock';
+				break;
+			case 1:
+				computerChoice = 'Paper';
+				break;
+			case 2:
+				computerChoice = 'Scissors';
+				break;
+		}
+		console.log('Computer: ' + computerChoice);
 	};
-	
-	return {
-		// public functions
-		myPublicFunction: function() {
-			
+
+	compare = function () {
+		if (userChoice === computerChoice) {
+			showResult.innerHTML = 'Game is a draw!';
+		} else if (userChoice === 'Paper' && computerChoice === 'Rock') {
+			showResult.innerHTML = 'You win!';
+		} else if (userChoice === 'Paper' && computerChoice === 'Scissors') {
+			showResult.innerHTML = 'Computer wins!';
+		} else if (userChoice === 'Scissors' && computerChoice === 'Rock') {
+			showResult.innerHTML = 'Computer wins!';
+		} else if (userChoice === 'Scissors' && computerChoice === 'Paper') {
+			showResult.innerHTML = 'You win!';
+		} else if (userChoice === 'Rock' && computerChoice === 'Scissors') {
+			showResult.innerHTML = 'You win!';
+		} else if (userChoice === 'Rock' && computerChoice === 'Paper') {
+			showResult.innerHTML = 'Computer wins!';
 		}
 	};
 
-	// gameModule end //
+	display = function () {
+		showUserChoice.innerHTML = userChoice;
+		showComputerChoice.innerHTML = computerChoice;
+	}
+
+	return {
+		// public functions
+		play: function () {
+			userChoice = this.id;
+			console.log('User: ' + userChoice);
+			computerInput();
+			display();
+			compare();
+		}
+	};
+
+	// module end //
 })();
 
-myGame.myPublicFunction();
+var buttons = document.getElementsByClassName('button');
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', myGame.play)
+}
